2531b0a13e70357f433727f0ff2d46f53f0c8b7d
Sebastian Pedernera <pedernera.sebastiandaniel@gmail.com>
2023-05-02 11:56:32 -0300

    Primeros comandos C++

---
+++ Basico C++/BasicoC++.cpp
@@ -0,0 +1,25 @@
+// primeros pasos en el lenguaje C++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout<< "¡¡ PRIMEROS PASOS C++ !!"<<endl;  // <<endl es salto de linea
+	cout<< "Tipo de datos en C++\n"; // terminar en \n es igual a enter o salto de linea
+	
+	// Definiendo las variables
+	int numero = 12;
+	float flotante = 12.5;
+	double mayor = 125.545;
+	char letra = 'a';
+	
+	// Imprimiendo en pantalla las variables
+	cout<<"El entero es: "<< numero<<endl;
+	cout<<"El float es: "<<flotante<<endl;
+	cout<<"El double es: "<<mayor<<endl;
+	cout<<"El char es: "<<letra;	
+	
+	return 0;
+}
