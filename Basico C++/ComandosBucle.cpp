709a1071fc1ed421859ad8972d733b8aac9972d3
Sebastian Pedernera <127348564+SebastianPedenera@users.noreply.github.com>
2023-05-08 16:14:29 -0300

    Teclab - Comandos de Bucles

--- Basico C++/ComandosBucle.cpp
+++ Basico C++/ComandosBucle.cpp
@@ -1,60 +1,36 @@
-/******* Hola Mundo
 #include <iostream>
 
-int main()
-{
-  std::cout << "Hello World!";
-}
-
-
-/**********contador
-#include <iostream>
-int main()
-{    
-    for (int i = 0; i < 5; i++){
+int main() {
+	
+	// Hola Mundo
+  	std::cout << "Hello World!\n";
+	
+	//contador	
+	for (int i = 0; i < 5; i++){
         std::cout << i << "\n";   
     }
-}
-
-/***********Ingresar un dato
-#include <iostream>
-
-int main()
-{
-    int n;
-    std::cout << "Ingrese un número: ";
-    std::cin >> n;
     
-    std::cout << "El valor es " << n;
-}
-
-/************verificar el valor del datos entre parametros
-#include <iostream>
-
-int main()
-{
+    //Ingresar un dato
     int n;
-    std::cout << "Ingrese un numero: ";
+    std::cout << "Ingrese un número: \n";
     std::cin >> n;
-    
-    std::cout << "El valor es " << n << "\n";
-    if (n == 3){
-        std::cout << "el numero justamente 3";
-    } else if (n < 3){
-        std::cout << "el numero es menor a 3";
+    std::cout << "El valor es " << n <<"\n";
+	
+	//verificar el valor del datos entre parametros
+    int n1;
+    std::cout << "Ingrese un numero: \n";
+    std::cin >> n1;
+    std::cout << "El valor es " << n1 <<"\n";
+    if (n1 == 3){
+        std::cout << "el numero justamente 3 \n";
+    } else if (n1 < 3){
+        std::cout << "el numero es menor a 3 \n";
     } else {
-        std::cout << "el numero es mayor a 3";
+        std::cout << "el numero es mayor a 3 \n";
     }
-}
 
-
-/****** switch
-
-#include <iostream>
-int main()
-{
+	// switch
     char grade = 'A';
-
     switch(grade) {
       case 'A' :
          std::cout << "Equivocado" << "\n";
@@ -72,17 +48,8 @@ int main()
       default :
          std::cout << "valor invalido" << "\n";
    }
-}
 
-
-
-
-/****** cilco while
-
-#include "iostream"
-
-int main()
-{
+	// cilco while
     int numero;
     std::cout <<  "Ingrese un numero ";
     std::cin >> numero;
@@ -92,17 +59,26 @@ int main()
         std::cin >> numero;
     }
     return true;
-}
- 
- 
-/**********obtener numeros al azar
-#include <iostream>
-
-int main()
-{
-    int a = 0;
+	
+	//obtener numeros al azar
+	/*
+	int a = 0;
     for (int i = 0; i < 5; i++){
-        a = rand()  % 10;
+        a = rand(i)  % 10;
         std::cout << a << "\n";
-    }
+    }	
+	*/
 }
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
